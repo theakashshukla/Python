d7d3526de9112a2ae9a626a95eff8bec756b5fdb
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-05-16 20:13:25 +0530

    done

--- 10.py
+++ 10.py
@@ -1,8 +1,8 @@
 # Two numbers are entered through the keyboard, write a program to find the value of one number raised to the power of another 
 
-a=input("Enter the first number: ")
-b=input("Enter the second number: ")
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
 
-c=a*b
+c=a**b
 
 print("The value of the first number raised to the power of the second number is: ",c)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+print("---------Akash Shukla---------")
+# a=int(input("Enter the first Number: "))
+# b=int(input("Enter The Second Number: "))
+# print("After The raised to the power of a^b: ", a**b)
