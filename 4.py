e47dba05551c6cf61b0b48a39fa52ec95533512f
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-07-15 09:30:41 +0530

    remove
    Co-Authored-By: Rakesh Ranjan Pradhan <101179634+Rakeshranjan25@users.noreply.github.com>

--- 4.py
+++ 4.py
@@ -1,6 +1,6 @@
 # Write a python script to print the current date in following format “Sun May 8 02:26:23 MCU 2022”
 import datetime
 
-print("------------Akash Shukla----------") 
+print("--------------Akash Shukla----------") 
 print("Current Date", datetime.datetime.
         now().strftime("%A %B %D %I:%M:%S MCU %Y"))
